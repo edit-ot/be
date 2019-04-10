6453de4d5b02246fd5024509bce676a600f7387b
eczn <1975845422@qq.com>
2019-04-11 05:09:23 +0800

    ~ i love hight order function

--- src/routes/doc/index.ts
+++ src/routes/doc/index.ts
@@ -90,27 +90,48 @@ router.post('/create', (req, res, next) => {
     }).catch(next);
 });
 
-router.post('/update', (req, res, next) => {
-    const session = req.session as StdSession;
-    const { user } = session;
+function CreateUpdateTask(
+    todo: (doc: Doc, ...args: Parameters<express.RequestHandler>) => void
+): express.RequestHandler {
+    return (req, res, next) => {
+        const session = req.session as StdSession;
+        const { user } = session;
 
-    Doc.findOne({
-        where: {
-            id: req.body.id
-        }
-    }).then(doc => {
-        if (doc && doc.owner === user.username) {
-            doc.title = req.body.title;
-            doc.save().then(() => {
-                res.json({ code: 200, data: doc, msg: 'updated' });
-            }).catch(next);
-        } else {
-            res.json({
-                code: 404
-            });
-        }
-    }).catch(next);
-})
+        Doc.findOne({
+            where: {
+                id: req.body.id
+            }
+        }).then(doc => {
+            if (doc && doc.owner === user.username) {
+                todo(doc, req, res, next);
+                doc.save().then(() => {
+                    res.json({ code: 200, data: doc, msg: 'updated' });
+                }).catch(next);
+            } else {
+                res.json({
+                    code: 404
+                });
+            }
+        }).catch(next);
+    }
+}
+
+
+router.post('/update', CreateUpdateTask((doc, req) => {
+    doc.title = req.body.title || '未命名标题';
+}))
+
+
+router.post('/save', CreateUpdateTask((doc, req) => {
+    if (req.body.title) {
+        doc.title = req.body.title
+    }
+
+    if (doc.content) {
+        doc.content = req.body.content;
+    }
+}))
+
 
 router.post('/delete', (req, res, next) => {
     const session = req.session as StdSession;
